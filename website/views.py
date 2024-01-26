38c6411c4cfaa749fc434622adce4872fd3fd728
sammels <sasagete@gmail.com>
2024-01-26 18:09:32 +0300

    feature: add index html

--- website/views.py
+++ website/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def main_page(request):
-    return render (request, 'index.html')
+    return render(request, 'index.html')
 
 def service(request):
     return render(request, 'service.html')
